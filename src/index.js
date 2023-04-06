c894ba392020059982d92e6d27a9efe0d60a6044
ryzvision <kandepatilkunal@gmail.com>
2023-04-06 10:07:13 +0530

    ✅

--- src/index.js
+++ src/index.js
@@ -18,11 +18,68 @@ const headers = () => {
     };
 };
 
-const createJob = async params => {
+const models = {
+    "sdv1_4.ckpt [7460a6fa]": true,
+    "anythingv3_0-pruned.ckpt [2700c435]": true,
+    "anything-v4.5-pruned.ckpt [65745d25]": true,
+    "analog-diffusion-1.0.ckpt [9ca13f02]": true,
+    "theallys-mix-ii-churned.safetensors [5d9225a4]": true,
+    "elldreths-vivid-mix.safetensors [342d9d26]": true
+}
+
+const ratio = {
+    "square": true,
+    "landscape": true,
+    "portrait": true
+}
+
+const samplers = {
+    "Euler": true,
+    "Euler a": true,
+    "Heun": true,
+    "DPM++ 2M Karras": true
+}
+
+const upscales = {
+    true: true,
+    false: true
+}
+
+const createJob = async ({ model, prompt, negative_prompt, seed, steps, cfg_scale, aspect_ratio, sampler, upscale }) => {
+    if (!models[model]) {
+        console.error(`[Error] Invalid model: ${model}`);
+        return;
+    }
+
+    if (!ratio[aspect_ratio]) {
+        console.error(`[Error] Invalid aspect ratio: ${aspect_ratio}`);
+        return;
+    }
+
+    if (!samplers[sampler]) {
+        console.error(`[Error] Invalid sampler: ${sampler}`);
+        return;
+    }
+
+    if (!upscales[upscale]) {
+        console.error(`[Error] Invalid upscale: ${upscale}`);
+        return;
+    }
+
     const response = await fetch(`${base}/job`, {
         method: "POST",
         headers: headers(),
-        body: JSON.stringify(params),
+        body: JSON.stringify({
+            model: model,
+            prompt: prompt,
+            negative_prompt: negative_prompt || "",
+            seed: seed || -1,
+            steps: steps || 10,
+            cfg_scale: cfg_scale || 7,
+            aspect_ratio: aspect_ratio || "square",
+            sampler: sampler || "",
+            upscale: upscale || false
+        }),
     });
 
     //Invalid API Key (401)
