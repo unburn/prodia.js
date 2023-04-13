65ef3d07fa17c5ff44b709e18cc60123d9a35127
Monty Anderson <work@montyanderson.net>
2023-04-13 07:00:57 -0700

    ✅

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,5 +1,18 @@
 declare module 'prodia-ai' {
-    type modelName = "sdv1_4.ckpt [7460a6fa]" | "v1-5-pruned-emaonly.ckpt [81761151]" | "anythingv3_0-pruned.ckpt [2700c435" | "anything-v4.5-pruned.ckpt [65745d25]" | "analog-diffusion-1.0.ckpt [9ca13f02]" | "theallys-mix-ii-churned.safetensors [5d9225a4]" | "elldreths-vivid-mix.safetensors [342d9d26]" | "openjourney_V4.ckpt [ca2f377f]" | "deliberate_v2.safetensors [10ec4b29]"
+    type modelName =
+        | "sdv1_4.ckpt [7460a6fa]"
+        | "v1-5-pruned-emaonly.ckpt [81761151]"
+        | "anythingv3_0-pruned.ckpt [2700c435"
+        | "anything-v4.5-pruned.ckpt [65745d25]"
+        | "analog-diffusion-1.0.ckpt [9ca13f02]"
+        | "theallys-mix-ii-churned.safetensors [5d9225a4]"
+        | "elldreths-vivid-mix.safetensors [342d9d26]"
+        | "deliberate_v2.safetensors [10ec4b29]"
+        | "openjourney_V4.ckpt [ca2f377f]"
+        | "portrait+1.0.safetensors [1400e684]" 
+        | "riffusion-model-v1.ckpt [3aafa6fe]"
+        | "timeless-1.0.ckpt [7c4971d4]";
+    
     type aspectRatio = "square" | "portrait" | "landscape";
     type sampler = "Euler" | "Euler a" | "Heun" | "DPM++ 2M Karras";
 
